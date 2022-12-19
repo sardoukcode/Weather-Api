71b17ddbb707ec3a58cd070ae669c1e2d67180bf
faysal-sardouk <sardouk29@gmail.com>
2022-12-19 22:44:13 +0200

    Working on the HTML CSS

--- weather-api/weather-api/src/App.js
+++ weather-api/weather-api/src/App.js
@@ -1,21 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>My name is Faysal</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Navbar">
+        <div className="Input-button-container">
+          <div className="location-input">
+            <input type={"text"} />
+          </div>
+          <div>
+            <button type="submit">Find Weather</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
